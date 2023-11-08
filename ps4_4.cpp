8a94cc4b71ff26eae4fb9d0ba66de86ce6403c68
unknown <harithsun34@gmail.com>
2023-11-08 15:17:29 +0530

    hi

---
+++ ps4_4.cpp
@@ -0,0 +1,20 @@
+#include <simplecpp>
+main_program {
+ 	int c1, c2, c3; int x1, y1, x2, y2, x3, y3 = 0;
+	float x, y, radius;
+	initCanvas("Circle", 800, 800);
+	c1 = getClick();
+       	x1 = c1/65536 ; y1 = c1 % 65536;
+	c2 = getClick();
+	x2 = c2/65536 ; y2 = c2 % 65536;
+	c3 = getClick();
+        x3 = c3/65536 ; y3 = c3 % 65536;
+	Circle p1(x1, y1, 5);
+	Circle  p2(x2, y2, 5);
+	Circle p3(x3, y3, 5);
+	x = float(((x1*x1 - x3*x3 + y1*y1 - y3*y3)*(2*(y1-y2)) -(x1*x1 - x2*x2 + y1*y1 - y2*y2)*(2*(y1-y3))))/(4*(((x2-x1)*(y1-y3))-((x3-x1)*(y1-y2))));
+	y= float((2*x*(x1-x2) + x2*x2-x1*x1 +  y2*y2 - y1*y1))/(2*(y2-y1));
+	radius = sqrt(pow((x-x1),2) + pow((y - y1), 2));
+	Circle circle(x, y, radius);
+	wait(5);
+}
